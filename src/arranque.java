e27253ba5ca2e72e103904d22636df7375d61504
JohnMayorga18 <45742970+JohnMayorga18@users.noreply.github.com>
2023-10-18 19:37:50 -0500

    Initial commit

---
+++ src/arranque.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author JohnMayorga18
+ */
+
+public class arranque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PanelVista jj = new PanelVista();
+        jj.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
